00781532df3a33f6a1bc9d99a4ebe05a4e1ad609
Ricardo A. Abreu <ricardo.abreu_ext@interplayers.com.br>
2022-10-31 15:39:11 -0300

    Fixed some layout issues

--- src/app/ui/components/stepper/stepper.component.ts
+++ src/app/ui/components/stepper/stepper.component.ts
@@ -1,27 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroupDirective,
-  NgForm,
-  Validators,
-} from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
-
-/** Error when invalid control is dirty, touched, or submitted. */
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(
-    control: FormControl | null,
-    form: FormGroupDirective | NgForm | null
-  ): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(
-      control &&
-      control.invalid &&
-      (control.dirty || control.touched || isSubmitted)
-    );
-  }
-}
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { MyErrorStateMatcher } from '../../models/error-state-matcher/error-state-matcher';
 
 @Component({
   selector: 'app-stepper',
@@ -36,10 +15,7 @@ export class StepperComponent implements OnInit {
     secondCtrl: ['', Validators.required],
   });
 
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
+  socialNumberFormControl = new FormControl('', [Validators.required]);
 
   matcher = new MyErrorStateMatcher();
 
